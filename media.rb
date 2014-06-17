f30ac67a8d23e96d23f748ded1002010d051cd11
Matthew Routley <matt@routleynet.org>
2014-06-17 10:25:23 -0400

    Add quality and reader incements

--- media.rb
+++ media.rb
@@ -3,11 +3,23 @@ require_relative 'basic_election_agent.rb'
 class Media < BasicElectionAgent
     
     attr_accessor :readership
+	attr_reader :quality
     
-    def initialize(readership, *basic_election_agent)
+    def initialize(readership, quality, *basic_election_agent)
         super basic_election_agent
         @readership = readership.to_i
+		@quality = quality.to_i || 5
     end
+	
+	def add_reader
+		change_readership_by(1)
+	end
+	
+	def remove_reader
+		change_readership_by(-1)
+	end
+	
+	private
 	
 	def change_readership_by(amount)
 		@readership += amount.to_i
