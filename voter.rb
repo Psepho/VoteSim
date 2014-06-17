0ce193da0d04798706e6a743185bc51f113e5866
Matthew Routley <matt@routleynet.org>
2014-06-17 10:54:49 -0400

    Add in placeholder methods

--- voter.rb
+++ voter.rb
@@ -12,5 +12,19 @@ class Voter < BasicElectionAgent
     def to_s
         puts "#{self.class} #{self.object_id} with age = #{age} and issues #{@issue_set}"
     end
-    
+	def subscribe
+		# find media, increment readership, add to media cashflow, and subtract from self cashflow
+	end
+	def unsubscribe
+		# find media, decrement readership, subtract from media cashflow, and add to self cashflow
+	end
+	def donate
+		# find candidate, subtract from self cashflow, and add to candidate cashflow
+	end
+    def choose_candidate
+		# Scan through issue_set of each candidate and calculate the sum of squared differences for each issue (position*weight). Then choose the candidate with the minimum value
+	end
+	def vote
+		# If past turnout threshold, vote for #choose_candidate
+	end
 end
