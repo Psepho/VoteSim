0ce8dcf604e759ecfa8e70d8e995f447ee32c1e9
mroutley <matt@routleynet.org>
2014-06-17 07:23:47 -0400

    Add readership instance variable

--- media.rb
+++ media.rb
@@ -2,8 +2,11 @@ require_relative 'basic_election_agent.rb'
 
 class Media < BasicElectionAgent
     
-    def initialize(location)
-        super(location)
+    attr_accessor :readership
+    
+    def initialize(readership, location, cashflow = 0, savings = 0, tractability = 0.1)
+        super(location, cashflow, savings, tractability)
+        @readership = readership.to_i
     end
     
 end
