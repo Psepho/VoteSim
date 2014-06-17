fe6eadb1ef126ba70774c16a4d521950f383ce42
Matthew Routley <matt@routleynet.org>
2014-06-17 11:12:04 -0400

    Add placeholder methods

--- candidate.rb
+++ candidate.rb
@@ -2,8 +2,21 @@ require_relative 'basic_election_agent.rb'
 
 class Candidate < BasicElectionAgent
     
-    def initialize(location, *basic_election_agent)
+    def initialize(name, name_recognition, *basic_election_agent)
         super basic_election_agent
+		@name = name.to_s
+		@name_recognition = name_recognition.to_i
     end
-    
+	def make_announcement
+		# Choose issue and make announcement, which costs $
+    end
+	def leave_election
+		# If savings too low, drop from race
+	end
+	def endorse_candidate
+		# If #leave_election, might choose another candidate
+	end
+	def adjust_name_recognition(amount)
+		@name_recognition += amount.to_i
+	end
 end
