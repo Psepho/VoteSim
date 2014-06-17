cf6f300b86bd430607c54c628a93fc9ad924cd6d
Matthew Routley <matt@routleynet.org>
2014-06-17 11:10:33 -0400

    Add voter_likeability

--- voter.rb
+++ voter.rb
@@ -3,11 +3,14 @@ require_relative 'basic_election_agent.rb'
 class Voter < BasicElectionAgent
     
     attr_reader :age, :income
+	attr_accessor :candidate_likeability
+	
     def initialize(age, income, *basic_election_agent)
         super basic_election_agent
         @age = age
         @income = income
         @location = location
+		@candidate_likeability = Hash.new
     end
     def to_s
         puts "#{self.class} #{self.object_id} with age = #{age} and issues #{@issue_set}"
@@ -22,9 +25,14 @@ class Voter < BasicElectionAgent
 		# find candidate, subtract from self cashflow, and add to candidate cashflow
 	end
     def choose_candidate
-		# Scan through issue_set of each candidate and calculate the sum of squared differences for each issue (position*weight). Then choose the candidate with the minimum value
+		# Scan through issue_set of each candidate and calculate the sum of squared differences for each issue Sum(position*weight)*candidate_likeability. Then choose the candidate with the minimum value
 	end
 	def vote
 		# If past turnout threshold, vote for #choose_candidate
+	end
+	def respond_to_poll
+	end
+	def adjust_candidate_likeability(candidate, amount)
+		@candidate_likeability[candidate.name] += amount
 	end
 end
