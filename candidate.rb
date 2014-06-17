625baf30be79fe6ecb1caa4a61b7938062bd9226
Matthew Routley <matt@routleynet.org>
2014-06-17 09:41:59 -0400

    Use splat argument

--- candidate.rb
+++ candidate.rb
@@ -2,8 +2,8 @@ require_relative 'basic_election_agent.rb'
 
 class Candidate < BasicElectionAgent
     
-    def initialize(location)
-        super(location)
+    def initialize(location, *basic_election_agent)
+        super basic_election_agent
     end
     
 end
