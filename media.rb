579de6c913476e70eff7c178ff526d9955cf779a
Matthew Routley <matt@routleynet.org>
2014-06-17 12:34:40 -0400

    More placeholder methods

--- media.rb
+++ media.rb
@@ -4,11 +4,12 @@ class Media < BasicElectionAgent
     
     attr_accessor :readership
 	attr_reader :quality
+	# tractability = 0
     
     def initialize(readership, quality, *basic_election_agent)
         super basic_election_agent
         @readership = readership.to_i
-		@quality = quality.to_i || 5
+		@quality = quality.to_f || 0.5
     end
 	def add_reader
 		change_readership_by(1)
@@ -17,11 +18,17 @@ class Media < BasicElectionAgent
 		change_readership_by(-1)
 	end
 	def conduct_poll
+		# Costs $
 	end
-	def write_article
-		# In response to candidate#announcement. Costs $ * quality
+	def report_news(type)
+		# In response to candidate#announcement or poll. Costs $ * quality
+		# Types include polls, articles, endorsements
+		# Increments name_recognition
 	end
 	def choose_candidate
+	end
+	def run_ad
+		# Paid for by candidate
 	end
 	
 	private
